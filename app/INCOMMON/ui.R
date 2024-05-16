f635b0413e29777d042df4e6ac7f06f209b86e87
nicola-calonaci <nicola.calonaci@gmail.com>
2024-05-16 17:48:07 +0200

    more fancy layout

--- app/INCOMMON/ui.R
+++ app/INCOMMON/ui.R
@@ -21,20 +21,39 @@ ui <- fluidPage(
   div(style = "background-color: #A9A9A7; padding: 10px;",
       titlePanel("Welcome to the INCOMMON ShinyApp", windowTitle = "INCOMMON")),
   div(style = "margin-top: 20px;",
-      p(HTML("Through this application you can use INCOMMON
-        to infer mutation copy number and multiplicity from
-        read counts data, interpret the mutant genomes in terms
-        of mutational and CNA status of TSGs and oncogenes,
-        perform survival and metastatic pattern analysis
-        using the intepreted genomes. Learn how to use INCOMMON at
-             <a href='https://caravagnalab.github.io/INCOMMON'>the INCOMMON website</a>",
+      p(HTML("INCOMMON is a tool for the INference of COpy number and Mutation Multiplicity
+      in ONcology. INCOMMON infers the copy number and multiplicity of somatic mutations
+      from tumor-only read count data, and can be applied to classify mutations from
+      large-size datasets in an efficient and fast way <a href='https://doi.org/10.1101/2024.05.13.24307238'>(Calonaci N., Krisniqi E. et al.)</a>.
+      <br/>
+      <br/>
+      INCOMMON offers a genome
+      interpretation framework, in which the full inactivation of tumor suppressor genes
+      (TSG) through mutations with LOH, and the enhanced activation of oncogenes through
+      mutations with amplification can be detected. These events can then be used to
+      perform augmented analysis of survival and metastatic patterns.
+      <br/>
+      <br/>
+      You can use this application to perform the following tasks:
+        <ul>
+        <li>Classification (Inference of mutation copy number and multiplicity)</li>
+        <li>Survival Analysis (Estimate hazard ratios based on INCOMMON interpreted genomes)</li>
+        <li>Metastatic propensity (Estimate odds ratios of metastasis based on INCOMMON interpreted genomes)</li>
+        <li>Metastatic organotropism (Estimate odds ratios of specific patterns of metastatic spread based on INCOMMON interpreted genomes)</li>
+        </ul>
+        <br/>
+        You can run these analyses on your own data, or download our data and results from our
+        <a href='https://zenodo.org/records/10927218'>Zenodo repository</a>.
+        <br/>
+        For more information about INCOMMON requirements, parameters and output, please visit
+             <a href='https://caravagnalab.github.io/INCOMMON'>the INCOMMON website</a>."
              )
         # tags$a(href = "https://caravagnalab.github.io/INCOMMON", "INCOMMON website", target = "_blank")
         )
       ),
   # Subtitle asking the user what they want to do
   div(style = "background-color: #B0BEC5; padding: 10px;",
-      h3("Select one workflow")),
+      h3("Select a task")),
   # # Action buttons to choose what to do
   # actionButton("classification_button", "Classification", ),
   # actionButton("survival_button", "Survival Analysis"),
@@ -42,62 +61,126 @@ ui <- fluidPage(
   # actionButton("met_tropism_button", "Metastatic Tropism"),
 
   # CSS to style the action buttons
-  tags$head(
-    tags$style(HTML("
-      .action-btn {
-        width: 200px;
-        height: 100px;
-        margin: 20px;
-        font-size: 18px;
-        border-radius: 10px;
-        position: relative;
-      }
-      .action-btn .tooltiptext {
-        visibility: hidden;
-        width: 220px;
-        background-color: #555;
-        color: #fff;
-        text-align: center;
-        border-radius: 6px;
-        padding: 10px;
-        position: absolute;
-        z-index: 1;
-        bottom: 125%;
-        left: 50%;
-        margin-left: -110px;
-        opacity: 0;
-        transition: opacity 0.3s;
-      }
-      .action-btn:hover .tooltiptext {
-        visibility: visible;
-        opacity: 1;
-      }
+  # tags$head(
+  #   tags$style(HTML("
+  #     .action-btn {
+  #       width: 200px;
+  #       height: 100px;
+  #       margin: 20px;
+  #       font-size: 18px;
+  #       border-radius: 10px;
+  #       position: relative;
+  #     }
+  #     .action-btn .tooltiptext {
+  #       visibility: hidden;
+  #       width: 220px;
+  #       background-color: #555;
+  #       color: #fff;
+  #       text-align: center;
+  #       border-radius: 6px;
+  #       padding: 10px;
+  #       position: absolute;
+  #       z-index: 1;
+  #       bottom: 125%;
+  #       left: 50%;
+  #       margin-left: -110px;
+  #       opacity: 0;
+  #       transition: opacity 0.3s;
+  #     }
+  #     .action-btn:hover .tooltiptext {
+  #       visibility: visible;
+  #       opacity: 1;
+  #     }
+  #
+  #     .classification-btn { background-color: #4CAF50; }
+  #     .survival-btn { background-color: #2196F3; }
+  #     .met-propensity-btn { background-color: #f44336; }
+  #     .met-tropism-btn { background-color: #ff9800; }
+  #   "))
+  # ),
 
-      .classification-btn { background-color: #4CAF50; }
-      .survival-btn { background-color: #2196F3; }
-      .met-propensity-btn { background-color: #f44336; }
-      .met-tropism-btn { background-color: #ff9800; }
-    "))
-  ),
+  tags$style(HTML(
+    ".classification-button {
+      background-image: url('classification.png');
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position-y: bottom;
+      background-position: bottom;
+      width: 220px;
+      height: 200px;
+      border: none;
+      cursor: pointer;
+    }
+
+    .survival-button {
+      background-image: url('survival.png');
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position-y: bottom;
+      background-position: bottom;
+      width: 220px;
+      height: 200px;
+      border: none;
+      cursor: pointer;
+    }
+
+    .metprop-button {
+      background-image: url('met_prop.png');
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position-y: bottom;
+      background-position: bottom;
+      width: 220px;
+      height: 200px;
+      border: none;
+      cursor: pointer;
+    }
+
+    .mettrop-button {
+      background-image: url('met_tropism.png');
+      background-size: contain;
+      background-repeat: no-repeat;
+      background-position-y: bottom;
+      background-position: bottom;
+      width: 220px;
+      height: 200px;
+      border: none;
+      cursor: pointer;
+    }
+    "
+  )),
 
   # Action buttons in two columns
   fluidRow(
     column(6, align = "center",
-           actionButton("classification_button", "Classification",
-                        class = "action-btn classification-btn",
-                        title = "Input: Data for classification.\nOutput: Predicted classes for the data.\n")),
+           actionButton(
+             inputId = "classification_button",
+             label = HTML("<b>Classification</b>"),
+             class = "classification-button",
+                        )),
     column(6, align = "center",
-           actionButton("survival_button", "Survival Analysis", class = "action-btn survival-btn",
-                        title = "Input: Survival data with covariates.\nOutput: Survival curves and hazard estimates.")))
-  ,
+           actionButton(
+             inputId = "survival_button",
+             label = HTML("<b>Survival Analysis</b>"),
+             class = "survival-button")
+           )
+    ),
   fluidRow(
-    column(6, align = "center",
-           actionButton("met_propensity_button", "Metastatic Propensity", class = "action-btn met-propensity-btn",
-                        title = "Input: Clinical and biological data.\nOutput: Predicted propensity of metastasis.")),
-    column(6, align = "center",
-           actionButton("met_tropism_button", "Metastatic Tropism", class = "action-btn met-tropism-btn",
-                        title = "Input: Tumor imaging data.\nOutput: Prediction of metastatic tropism.")))
-  ,
+    column(6,
+           align = "center",
+           actionButton(
+             "met_propensity_button",
+             HTML("<b>Metastatic Propensity</b>"),
+             class = "mettrop-button")
+           ),
+    column(6,
+           align = "center",
+           actionButton(
+             "met_tropism_button",
+             HTML("<b>Metastatic Tropism</b>"),
+             class = "metprop-button")
+           )
+    ),
 
   # Placeholder for rendering the selected shiny app module
   uiOutput("selected_module")
